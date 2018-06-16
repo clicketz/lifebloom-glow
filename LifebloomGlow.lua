c61d5f78e39b5bda7a17b217e29fae94e2669b81
clicketz <40314230+clicketz@users.noreply.github.com>
2018-06-16 15:45:41 -0600

    Update LifebloomGlow.lua

--- LifebloomGlow.lua
+++ LifebloomGlow.lua
@@ -1,8 +1,8 @@
-local _,playerClass = UnitClass("player")
-local CF = CreateFrame("Frame")
-CF:RegisterEvent("PLAYER_LOGIN")
+local _, playerClass = UnitClass("player")
+local cf = CreateFrame("Frame")
+cf:RegisterEvent("PLAYER_LOGIN")
 
-CF:SetScript("OnEvent", function(self, event)
+cf:SetScript("OnEvent", function(self, event)
 
 	if (playerClass ~= "DRUID") then
 		print("|cFF50C878L|rifebloom |cFF50C878G|rlow Disabled")
@@ -14,27 +14,27 @@ CF:SetScript("OnEvent", function(self, event)
 		local buffName, _, icon, count, debuffType, duration, expirationTime, caster, canStealOrPurge, nameplateShowPersonal,
 		spellId, canApplyAura, isBossDebuff, casterIsPlayer, nameplateShowAll, timeMod, _ = UnitBuff(unit, index, filter)
 
-		if spellId == 33763 and casterIsPlayer then
+		if (spellId == 33763 and casterIsPlayer) then
 			local timeRemaining = (expirationTime - GetTime()) / timeMod
 			local refreshTime = duration * 0.3
 
-			if not buffFrame.highlight then
-				local highlight = buffFrame:CreateTexture(nil, "OVERLAY")
-				highlight:SetTexture([[Interface\TargetingFrame\UI-TargetingFrame-Stealable]])
-				highlight:SetPoint("TOPLEFT", -3, 3)
-				highlight:SetPoint("BOTTOMRIGHT", 3, -3)
-				highlight:SetBlendMode("ADD")
-				buffFrame.highlight = highlight
+			if (not buffFrame.glow) then
+				local glow = buffFrame:CreateTexture(nil, "OVERLAY")
+				glow:SetTexture([[Interface\TargetingFrame\UI-TargetingFrame-Stealable]])
+				glow:SetPoint("TOPLEFT", -3, 3)
+				glow:SetPoint("BOTTOMRIGHT", 3, -3)
+				glow:SetBlendMode("ADD")
+				buffFrame.glow = glow
 			end
 
-			if timeRemaining <= refreshTime then
-				buffFrame.highlight:Show()
+			if (timeRemaining <= refreshTime) then
+				buffFrame.glow:Show()
 			else
-				buffFrame.highlight:Hide()
+				buffFrame.glow:Hide()
 			end
 
-		elseif buffFrame.highlight then
-			buffFrame.highlight:Hide()
+		elseif buffFrame.glow then
+			buffFrame.glow:Hide()
 		end
 	end
 	hooksecurefunc("CompactUnitFrame_UtilSetBuff", CompactUnitFrame_UtilSetBuff_Hook)
@@ -49,15 +49,15 @@ CF:SetScript("OnEvent", function(self, event)
 		spellId, canApplyAura, isBossDebuff, casterIsPlayer, nameplateShowAll, timeMod, _ = UnitBuff(self.unit, i, nil)
 
 			if buffName then
-				if spellId == 33763 and casterIsPlayer then
+				if (spellId == 33763 and casterIsPlayer) then
 					frameName = selfName.."Buff"..(i)
 					frame = _G[frameName]
-					if frame and icon and (not self.maxBuffs or i <= self.maxBuffs) then
+					if (frame and icon and (not self.maxBuffs or i <= self.maxBuffs)) then
 						local timeRemaining = (expirationTime - GetTime()) / timeMod
 						local refreshTime = duration * 0.3
 						local frameStealable = _G[frameName.."Stealable"]
 
-						if timeRemaining <= refreshTime then
+						if (timeRemaining <= refreshTime) then
 							frameStealable:Show()
 						else
 							frameStealable:Hide()
