65d3f31e30809432f14f1da43fbb73d6478ab34e
clicketz <40314230+clicketz@users.noreply.github.com>
2022-11-06 14:43:36 -0700

    Use geq for cachedTick check

--- LifebloomGlow.lua
+++ LifebloomGlow.lua
@@ -207,7 +207,7 @@ local function glowIfSotf(aura, buffFrame)
 
     -- Workaround for Overgrowth. It always applies SotF to rejuv/germ and also at the base tick rate.
     if addon.overgrowth and (sId == 774 or sId == 155777) then
-        if curTick > cachedTick * mult then
+        if curTick >= cachedTick * mult then
             glowSotf(buffFrame, aura, curTick, true)
         end
         return
